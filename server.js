3105e2348fc4c7928d594ab8532a050f21fd82cd
Anas <anasanchu3004@gmail.com>
2024-04-22 16:25:44 +0530

    Connect express to mongodb atlas using mongoose

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB CONNECTED SUCCESSFULLY"))
+  .catch((error) => console.log(error));
 
-
-app.listen(PORT,()=>{
-    console.log(`Server started on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
