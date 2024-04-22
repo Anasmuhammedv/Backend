1d418d9932d64019d5d96d19f48c5f74777353a7
Anas <anasanchu3004@gmail.com>
2024-04-22 12:27:52 +0530

    started backend project create a server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 4000
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server started on http://localhost:${PORT}`)
+})
